bf8a3bd8f695240cd6ecee698b27c147556e948e
Dipanjon <dipanjon95@gmail.com>
2025-03-20 18:54:59 +0600

    Error handeler done

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const connectToDB = require("./dbConfig/connectToDb");
+const ExpressError = require("../../../Uber/backend/src/utility/expresserror");
 
 connectToDB();
 
@@ -11,18 +12,34 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cookieParser());
-app.use(cors({
-  origins: "http://localhost:5173",
-  methods: ["GET", "POST", "DELETE", "PUT"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Cache-Control",
-    "Expires",
-    "Pragma"
-  ],
-  credentials: true
-}))
+app.use(
+  cors({
+    origins: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+//Error handeling
+
+app.use("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not found."));
+});
+
+app.use((err, req, res, next) => {
+  const { status = 501, message = "Server side issue" } = err;
+  res.status(status).json({
+    success: false,
+    message,
+  });
+});
 
 app.listen(port, () => {
   console.log("Server is running from port", port);
